f246f489ce915c2131e25975c1d219d84aa6a106
Nathaniel Angus <angusn@allegheny.edu>
2022-01-08 17:10:55 -0500

    Added basic html, css, and js for the app

---
+++ website/app.js
@@ -0,0 +1,10 @@
+// Selectors
+const searchIcon = document.querySelector(".search-icon");
+
+// Event Listeners
+searchIcon.addEventListener("click", searchHeroes);
+
+// Functions
+function searchHeroes() {
+  console.log("Search conducted");
+}
