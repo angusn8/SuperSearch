3644f2dd00fa32bf2c9d00f36506b464da48b901
Nathaniel Angus <angusn@allegheny.edu>
2022-01-11 13:28:47 -0500

    Started styling

--- website/app.js
+++ website/app.js
@@ -1,10 +1,12 @@
 // Selectors
 const searchInput = document.querySelector(".search-input");
 const searchBtn = document.querySelector(".search-btn");
+const searchForm = document.querySelector("#search-form");
 const heroList = document.querySelector(".hero-list");
 
 // Event Listeners
 searchBtn.addEventListener("click", loadHeroes);
+searchForm.addEventListener("submit", loadHeroes);
 
 // Functions
 async function loadHeroes() {
@@ -26,13 +28,14 @@ function displayHeroes(heroes) {
 
   const htmlString = heroes.map((hero) => {
     return `
-      <li class="hero col-sm-3">
+      <li class="hero col-md-4">
         <img src="${hero.image.url}" class="hero-img">
         <h2>${hero.name}</h2>
-        <h4>${hero.biography['full-name']}</h4>
+        <h4 class="hero-subtext">Full Name: ${hero.biography['full-name']}</h4>
+        <h4 class="hero-subtext">Publisher: ${hero.biography['publisher']}</h4>
       </li>
     `;
-  }).join();
+  }).join("");
 
   heroList.innerHTML = htmlString;
 
